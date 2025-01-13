33c519d6a4951f1514af2ef238fdbd74d08274aa
sbhuvan455 <sbhuvan455@gmail.com>
2025-01-13 13:35:14 +0530

    Add ReactPlayer for video streaming and update WebSocket handling in Room component

--- frontend/src/component/Room.jsx
+++ frontend/src/component/Room.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSocket } from '../contexts/socketContext';
 import { Answer, CreateRoom, Offer, RoomCreated, SendAnswer, SendOffer } from '../types';
 import { useState } from 'react';
 import { usePeerContext } from '../contexts/peerContext';
+import ReactPlayer from 'react-player'
 
 function Room() {
 
     const [waiting, setWaiting] = useState(true);
+    const [myStream, setMyStream] = useState(null);
     const { peer, createOffer } = usePeerContext();
+    const [remoteStream, setRemoteStream] = useState();
 
     const { room } = useParams();
     const socket = useSocket();
@@ -20,11 +23,20 @@ function Room() {
         return answer;
     }
 
+    const startCall = useCallback(() => {
+        console.log("Inside startCall", myStream);
+        for (const tracks of myStream.getTracks()){
+            peer.peer.addTrack(tracks, myStream);
+        }
+    }, [myStream])
+
     const storeAnswer = async (answer) => {
         await peer.setRemoteDescription(answer);
+
+        startCall();
     }
 
-    const handleMessage = (data) => {
+    const handleMessage = async (data) => {
         // console.log(data)
         const message = JSON.parse(data.data);
 
@@ -33,21 +45,33 @@ function Room() {
         switch(message.type){
             case RoomCreated:
                 setWaiting(false);
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    video: true,
+                    audio: true
+                })
+
+                setMyStream(stream);
+
                 if(message.payload.canSendOffer){
-                    const offer = createOffer();
+                    const offer = await createOffer();
+                    console.log("Have created a new offer", offer);
                     socket.send(JSON.stringify({
                         type: SendOffer,
                         payload: offer
                     }))
                 }
+
+                break;
             case Offer:
-                const answer = createAnswer(message.payload);
+                const answer = await createAnswer(message.payload);
                 socket.send(JSON.stringify({
                     type: SendAnswer,
                     payload: answer
                 }))
+                break;
             case Answer:
                 storeAnswer(message.payload);
+                break;
         }
     }
 
@@ -72,6 +96,20 @@ function Room() {
 
     }, [room, socket]);
 
+    useEffect(() => {
+        const handleTrack = async (event) => {
+            const [stream] = event.streams;
+            setRemoteStream((prev) => prev || stream);
+        };
+    
+        peer?.peer?.addEventListener("track", handleTrack);
+    
+        return () => {
+            peer?.peer?.removeEventListener("track", handleTrack);
+        };
+    }, [peer]);
+    
+
     if(waiting) {
         console.log(waiting);
         return (
@@ -84,6 +122,24 @@ function Room() {
     return (
         <div>
             {room}
+            {(myStream) && 
+            <ReactPlayer 
+                width={800} 
+                height={800} 
+                playsinline 
+                playing={true}
+                url={myStream}
+            />
+            }
+            {(remoteStream) && 
+                <ReactPlayer
+                    width={300}
+                    height={300}
+                    playsinline
+                    playing={true}
+                    url={remoteStream}
+                />
+            }
         </div>
     )
 }
